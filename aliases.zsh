c2ba34f733322a19486be750fdb9a6fb0dc77215
damianrivas <drivas1993@gmail.com>
2023-05-07 23:04:15 -0500

    Replace ls with exa. Add script updates

--- aliases.zsh
+++ aliases.zsh
@@ -1,4 +1,12 @@
+alias ls="exa"
+alias l="exa -ls type --icons"
+alias ll="exa -las type --icons"
+
+alias sysupdate="sudo apt update && sudo apt upgrade"
+alias scriptsupdate="env -C $ZSH_CUSTOM/scripts find . -maxdepth 1 -type f -exec chmod 775 {} \;"
+
 alias zshconfig="nano ~/.zshrc"
 alias reloadzsh="source ~/.zshrc"
 alias updatezshconfig="env -C $ZSH_CUSTOM git-pull-diff"
+
 alias gotoprojects="cd ~/projects"
