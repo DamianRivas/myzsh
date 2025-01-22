eca6f249a24cc72736d18495e85c7fc7c156d7de
Damian Rivas <drivas1993@gmail.com>
2025-01-22 09:53:52 -0800

    Replace exa with eza

--- aliases.zsh
+++ aliases.zsh
@@ -1,7 +1,7 @@
 alias bcat="batcat"
-alias ls="exa"
-alias l="exa -ls type --icons"
-alias ll="exa -las type --icons"
+alias ls="eza"
+alias l="eza -ls type --icons"
+alias ll="eza -las type --icons"
 
 alias sysupdate="sudo apt update && sudo apt upgrade"
 alias scriptsupdate="env -C $ZSH_CUSTOM/scripts find . -maxdepth 1 -type f -exec chmod 775 {} \;"
